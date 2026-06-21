eddc87e9df50edaaa8dd86cdc06ec476da39dac1
shah1nyananna <annashahinyan._20@icloud.com>
2026-06-21 15:19:05 +0400

    init

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,7 +1,12 @@
 public class Lesson1 {
 
-
-
-
+    public static void main(String[] args) {
+        char x = '*';
+        for (int i = 0; i < 5; i++) {
+            for (int y = 0; y <= i; y++) {
+                System.out.print(x);
+            }
+            System.out.println();
+        }
+    }
 }
-
