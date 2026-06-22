8d53ee8a7ef387e65f9703fc81a9141fc945d873
shah1nyananna <annashahinyan._20@icloud.com>
2026-06-22 16:16:37 +0400

    Lesson 1

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,21 +1,27 @@
 public class Lesson1 {
 
     public static void main(String[] args) {
-        char a = '*';
+        char o = '*';
         for (int p = 0; p <= 5; p++) {
             for (int k = 5; k >= p; k--) {
-                System.out.print(a);
+                System.out.print(o);
             }
             System.out.println();
         }
-
-
-
-
-
-
+        System.out.println("++++++");
+        for (int t = 0; t < 5; t++) {
+            for (int s = 0; s < 4 - t; s++) {
+                System.out.print(" ");
+            }
+            for (int d = 0; d <= 2 * t; d++) {
+                System.out.print(o);
+            }
+            System.out.println();
+        }
     }
 }
+
+
 
 
 
