7e3cd14dbe917c60e223c4660ba8a408eeef2b40
Ani Kovalenko <ani_kovalenko@epam.com>
2026-06-23 17:33:10 +0400

    Set homework 2

--- src/lesson1/Lesson1.java
+++ src/lesson1/Lesson1.java
@@ -1,3 +1,5 @@
+package lesson1;
+
 public class Lesson1 {
 
     public static void main(String[] args) {
---
+++ src/lesson2/SeasonsHomework.java
@@ -0,0 +1,33 @@
+package lesson2;
+
+public class SeasonsHomework {
+
+    /**
+     * Ունեք թվերի մասիվ, որոնք պետք է ունենան 1-12 արժեքներ
+     * int[] months = {1, 3, 6, 9, 12};
+     *
+     * Գրեք կոդի հատված, որը կտպի մասիվի ԲՈԼՈՐ թվերին համապատասխան ամիսների անունները
+     *
+     * Օրինակ՝ մասիվի մեջ ունենալով 3 թիվը, պետք է ակնկալել Մարտ ամիսը ․․․
+     * Խնդիրը լուծել 2 եղանակով՝
+     *
+     * Լուծում 1:
+     * for-i , if-else if
+     *
+     * Լուծում 2:
+     * foreach, switch
+     */
+    public static void main(String[] args) {
+//        solution 1: fori + if
+
+
+
+
+//        solution 2: foreach + switch
+
+
+
+
+
+    }
+}
