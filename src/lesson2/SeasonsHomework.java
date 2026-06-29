6c054be9c5c22e235c544a7ec9f30d6946313171
shah1nyananna <annashahinyan._20@icloud.com>
2026-06-29 14:07:25 +0400

    Lesson2

--- src/lesson2/SeasonsHomework.java
+++ src/lesson2/SeasonsHomework.java
@@ -30,9 +30,55 @@ public class SeasonsHomework {
             } else if (month == 9) {
                 System.out.println("September");
             } else if (month == 12) {
-                System.out.println("December");
+               System.out.println("December");
+           }
+    }
+
+
+        for (int m : months) {
+            switch (m) {
+                case 1:
+                    System.out.println("January");
+                    break;
+                case 2:
+                    System.out.println("Fabruary");
+                    break;
+                case 3:
+                    System.out.println("March");
+                    break;
+                case 4:
+                    System.out.println("April");
+                    break;
+                case 5:
+                    System.out.println("May");
+                    break;
+                case 6:
+                    System.out.println("June");
+                    break;
+                case 7:
+                    System.out.println("July");
+                    break;
+                case 8:
+                    System.out.println("August");
+                    break;
+                case 9:
+                    System.out.println("September");
+                    break;
+                case 10:
+                    System.out.println("October");
+                    break;
+                case 11:
+                    System.out.println("November");
+                    break;
+                case 12:
+                    System.out.println("December");
+                    break;
             }
         }
     }
 }
+
+
+
+
 
